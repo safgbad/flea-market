b842d48cf799004aea440fd8fcb3823143cf1373
Artem Nazarov <art.nazar.11@gmail.com>
2023-08-09 19:51:08 +0300

    GH-13 removed odd overrides in repository

--- src/main/java/ru/skypro/flea/repository/AdRepository.java
+++ src/main/java/ru/skypro/flea/repository/AdRepository.java
@@ -1,23 +1,9 @@
 package ru.skypro.flea.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import ru.skypro.flea.model.Ad;
 
-import java.util.Optional;
-
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Integer> {
-
-    @Override
-    @NonNull
-    Optional<Ad> findById(@NonNull Integer id);
-
-    @Override
-    boolean existsById(@NonNull Integer integer);
-
-    @Override
-    void deleteById(@NonNull Integer integer);
-
 }
