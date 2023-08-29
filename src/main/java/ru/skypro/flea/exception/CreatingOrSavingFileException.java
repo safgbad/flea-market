3a486011e897171e9083ebcc4ed8bc6d924910ce
Artem Nazarov <art.nazar.11@gmail.com>
2023-08-29 19:11:47 +0300

    handling UnsupportedImageTypeException

--- src/main/java/ru/skypro/flea/exception/CreatingOrSavingFileException.java
+++
@@ -1,8 +0,0 @@
-package ru.skypro.flea.exception;
-
-public class CreatingOrSavingFileException  extends RuntimeException {
-
-  public CreatingOrSavingFileException(String message) {
-    super(message);
-  }
-}
--- src/main/java/ru/skypro/flea/exception/handler/CommonExceptionHandler.java
+++ src/main/java/ru/skypro/flea/exception/handler/CommonExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.skypro.flea.exception.FileSystemError;
 import ru.skypro.flea.exception.ResourceWithSpecifiedIdNotFoundException;
 import ru.skypro.flea.exception.RestError;
+import ru.skypro.flea.exception.UnsupportedImageTypeException;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
@@ -21,6 +22,12 @@ public class CommonExceptionHandler {
     public ResponseEntity<RestError> handleFileSystemError(
             FileSystemError ex) {
         return configureResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex);
+    }
+
+    @ExceptionHandler(UnsupportedImageTypeException.class)
+    public ResponseEntity<RestError> handleUnsupportedImageTypeException(
+            UnsupportedImageTypeException ex) {
+        return configureResponse(HttpStatus.BAD_REQUEST, ex);
     }
 
     private ResponseEntity<RestError> configureResponse(
